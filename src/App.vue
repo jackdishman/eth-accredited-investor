824bf19c9706a372503bf5c1f3519fdac8dd3a56
jack dishman <me@jackdishman.com>
2021-12-11 11:10:09 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { ref } from "Vue";
+
+const searchValue = ref(``);
+
+function search(): void {
+	console.log(searchValue.value);
+}
+</script>
+
+<template>
+	<input
+		type="text"
+		v-model="searchValue"
+		placeholder="Enter ETH address or .ens"
+	/>
+	<button @click="search">Search</button>
+</template>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
